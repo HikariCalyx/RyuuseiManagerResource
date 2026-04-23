e8dd1349d5712a79e1715fd2d372c7f3aa5725bf
Calyx Hikari <29157608+HikariCalyx@users.noreply.github.com>
2026-04-23 22:50:11 +0800

    Add dummyClass

---
+++ DummyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace GameResource
+{
+    internal class DummyClass
+    {
+        private void Preload()
+        {
+        }
+    }
+}
